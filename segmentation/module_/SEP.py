d2a435ec82d60ad5302999436fbd6a70ef494022
kisookim <k014520@kaist.ac.kr>
2021-10-30 00:09:01 +0900

    add code for SEP

--- segmentation/module_/SEP.py
+++ segmentation/module_/SEP.py
@@ -2,11 +2,13 @@ import numpy as np
 import sys
 from scipy.spatial import distance_matrix
 
-class DensityRatioSEP: 
+class DensityRatioSEP:
 
-    # ref_data: x_{t-1} -> f_{t-1}(x)   (shape: (data_len, feature_len))
-    # test_data: x_{t} -> f_{t}(x)      (shape: (data_len, feature_len))
-    # ref_data and test_data are two consecutive windows
+    """
+        * ref_data: x_{t-1} -> f_{t-1}(x)   (shape: (data_len, feature_len))
+        * test_data: x_{t} -> f_{t}(x)      (shape: (data_len, feature_len))
+        * ref_data and test_data are two consecutive windows
+    """
 
     def __init__(self, ref_data, test_data):
 
@@ -19,8 +21,7 @@ class DensityRatioSEP:
 
         # implementation configurations: https://www.sciencedirect.com/science/article/pii/S0893608013000270?via%3Dihub
         # Liu et al., Change-point detection in time-series data by relative density-ratio estimation
-        total_samples = np.concatenate([ref_data, test_data], axis=0)
-        median_distance = self.compute_median_distance(total_samples)
+        median_distance = self.compute_median_distance(np.concatenate([ref_data, test_data], axis=0))
 
         sigma_range = median_distance * np.array([0.6, 0.8, 1., 1.2, 1.4])
         lambda_range = 10. ** np.array([-3, -2, -1, 0, 1])
@@ -33,76 +34,54 @@ class DensityRatioSEP:
         self._SEP(ref_data, test_data)
 
     def compute_median_distance(self, samples):
-        
+
+        assert samples.shape[0] == 2*self.__length
         distances = distance_matrix(samples, samples)
         distances = np.tril(distances).reshape((1, -1))
 
         median_distance = np.sqrt(0.5) * np.median(distances[distances>0])
 
-        if np.isnan(median_distance):
-            median_distance = 0.
-
-        return median_distance if median_distance != 0. else 0.1
+        return median_distance if median_distance != 0 and (not np.isnan(median_distance)) else 0.1
             
     def cross_validation(self, ref_data, test_data, sigmas, lambdas):
 
         n = self.__length
-
         optimal_sigma = optimal_lambda = 0.
         opt_score = sys.maxsize
 
-        one_nT=np.matrix(np.ones(n))    # (1, n)
-        one_bT=np.matrix(np.ones(n))    # (1, b)
+        one_nT = np.matrix(np.ones(n))    # (1, n)
+        one_bT = np.matrix(np.ones(n))    # (1, b)
 
         for sigma_ in sigmas:
 
             phi_ref = self.kernel_matrix(ref_data, self.__kernel_centers,  sigma_)
             phi_test = self.kernel_matrix(test_data, self.__kernel_centers,  sigma_)
-            assert phi_ref.shape==(self.__length, self.__length)
 
-            # h_hat = np.prod(phi_test, axis=0).reshape((-1, 1))#/self.__length # (1, n)
-            # h_hat = (np.prod(phi_test, axis=1)/self.__length).reshape((1, -1)) # 1-by-test
-            # h_hat = np.mean(phi_test, axis=1).reshape((-1, 1)) # (1, n), https://link.springer.com/content/pdf/10.1007/s40860-018-0065-2.pdf
-            # assert h_hat.shape==(1, self.__length)
-
+            # H = phi_test@phi_test.T/n # (b, b)
             h_hat = np.mean(phi_test, axis=0).T
 
             for lambda_ in lambdas:
 
-                B=-np.identity(n)*lambda_*(n-1)/n # (b, b)
-                # B=(-lambda_candidate*np.identity(self.__kernel_num))*(self.__train_n-1)/self.__train_n
-                BinvPtr=np.linalg.solve(B, phi_test) # (b, b)@(b, n) -> (b, n) # invCK_de
-                # beta = np.linalg.solve(B, h) # (b, b)@(b, 1) -> (b, 1)
-                PtrBinvPtr=np.multiply(phi_test, BinvPtr) # (b, n) element-wise multiplication
-                denominator=n*one_nT - one_bT@PtrBinvPtr # (1, b)@(b, n) -> (1, n) # tmp
+                # B = H + np.identity(n)*lambda_*(n-1)/n # (b, b)
+                B = np.identity(n)*lambda_*(n-1)/n
+            
+                BinvPtr = np.linalg.solve(B, phi_test) # (b, b)@(b, n) -> (b, n) # invCK_de
+                PtrBinvPtr = np.multiply(phi_test, BinvPtr) # (b, n) element-wise multiplication
+                denominator = n*one_nT - one_bT@PtrBinvPtr # (1, b)@(b, n) -> (1, n) # tmp
 
-                diagonal_B0=np.diag((h_hat.T@BinvPtr).A1/denominator.A1) # (1, b)@(b, n) -> (1, n) -> (n, n)
-                # diagonal_B0=np.diag((beta.T@phi_train).A1/denominator.A1) # (1, b)@(b, n) -> (1, n) -> (n, n)
-                B0=np.linalg.solve(B, h_hat@one_nT) + BinvPtr@diagonal_B0 # (b,b)@(b, 1)@(1, n) -> (b,n)
+                diagonal_B0 = np.diag((h_hat.T@BinvPtr).A1/denominator.A1) # (1, b)@(b, n) -> (1, n) -> (n, n)
+                B0 = np.linalg.solve(B, h_hat@one_nT) + BinvPtr@diagonal_B0 # (b,b)@(b, 1)@(1, n) -> (b,n)
 
-                diagonal_B1=np.diag((one_bT@(np.multiply(phi_ref, BinvPtr))).A1/denominator.A1) # (1, b)@(b, n)->(1,n) -> (n,n)
-                B1=np.linalg.solve(B, phi_ref) + BinvPtr@diagonal_B1 # (b,n)@(n,n)->(b,n) // (b,b)@(b,n) -> (b,n)
+                diagonal_B1 = np.diag((one_bT@(np.multiply(phi_ref, BinvPtr))).A1/denominator.A1) # (1, b)@(b, n)->(1,n) -> (n,n)
+                B1 = np.linalg.solve(B, phi_ref) + BinvPtr@diagonal_B1 # (b,n)@(n,n)->(b,n) // (b,b)@(b,n) -> (b,n)
 
-                B2=(n-1)*(n*B0-B1)/(n*(n-1)) # (b, n)
-                B2[B2<0]=0
+                B2 = (n-1)*(n*B0-B1)/(n*(n-1)) # (b, n)
+                B2[B2<0] = 0
 
-                w_test=(one_bT@(np.multiply(phi_test, B2))).T # (1, b)@(b,n)->(1,n)->(n,1) 
-                w_ref=(one_bT@(np.multiply(phi_ref, B2))).T # (1,b)@(b,n) -> (1,n) -> (n,1)
+                w_test = (one_bT@(np.multiply(phi_test, B2))).T # (1, b)@(b,n)->(1,n)->(n,1)
+                w_ref = (one_bT@(np.multiply(phi_ref, B2))).T # (1,b)@(b,n) -> (1,n) -> (n,1)
 
-                objective_score = max(0, 0.5-w_ref.mean())
-
-                # theta = (h_hat/lambda_) *-1 # (1, n)
-                # right_term = np.prod(phi_ref, axis=0).reshape((1, -1))
-                # assert theta.shape==right_term.shape
-                # density_ratio = np.multiply(theta, right_term).reshape(1,-1)
-                # # right_term = np.prod(phi_test, axis=1)
-                # # right_term = np.diagflat(right_term)
-                # # assert right_term.shape==(n, n)
-                # # density_ratio = theta@phi_test.T # (1, n)@(n, n) => (1, n)
-                # # density_ratio = kernel
-                # # assert density_ratio.shape==(1, n)
-                # objective_score = max(0, 0.5 - density_ratio.mean())
-                # # objective_score = abs(0.5-np.sum(density_ratio)/n)
+                objective_score = abs(0.5 - w_ref.mean())
 
                 if objective_score < opt_score:
                     opt_score = objective_score
@@ -115,50 +94,32 @@ class DensityRatioSEP:
         
         n = self.__length
 
-        # phi_ref = self.kernel_matrix(self.__kernel_centers, ref_data, self.__sigma_)
         phi_ref = self.kernel_matrix(ref_data, self.__kernel_centers, self.__sigma_)
-        # phi_test = self.kernel_matrix(self.__kernel_centers, test_data, self.__sigma_)
         phi_test = self.kernel_matrix(test_data, self.__kernel_centers, self.__sigma_)
-        assert phi_ref.shape==(self.__length, self.__length)
 
-        # h_hat = (np.prod(phi_test, axis=1)/self.__length).reshape((1, -1)) # 1-by-test
-        # h_hat = np.mean(phi_ref, axis=1).reshape((1,-1))
-        # h_hat = np.mean(phi_test, axis=1).reshape((-1, 1)) # (n, 1), https://link.springer.com/content/pdf/10.1007/s40860-018-0065-2.pdf
-        # h_hat = np.prod(phi_test, axis=0).reshape((-1, 1))#/self.__length # (1, n)
+        assert phi_ref.shape==(self.__length, self.__length)
 
         h_hat = np.mean(phi_test, axis=0).T # mean of all centers of each test datum
 
-        # theta = h_hat/self.__lambda_ #* -1
-        theta = np.linalg.solve(np.identity(n)*self.__lambda_, h_hat)
-        # theta[theta<0] = 0
+        # theta = np.linalg.solve(np.identity(n)*self.__lambda_, h_hat)
+        theta = h_hat/self.__lambda_
+        theta[theta<0] = 0
 
         self.__kernel_matrix = phi_test
         self.__h_hat = h_hat
         self.__theta = theta
 
-        # density_ratio = phi_test.T@theta
-
-        density_ratio = np.multiply(
-            np.prod(phi_ref, axis=0), theta
-        )
+        density_ratio = phi_ref.T@theta
 
         assert density_ratio.shape==theta.shape
 
-        # right_term = np.prod(phi_test, axis=0).reshape((1, -1))
-        # right_term = np.diagflat(right_term)
-        # assert right_term.shape==(n, n)    
-        # density_ratio = np.multiply(theta, right_term).reshape(1,-1)
-        # density_ratio = theta@phi_test.T # (1, n)@(n, n) => (1, n)
-
         score = max(0, 0.5 - density_ratio.mean())
-        # score = abs(0.5 - np.sum(density_ratio) / n)
         
         self.__density_ratio = density_ratio
         self.__score = score
         
 
     def kernel_matrix(self, data, centers, sigma):
-
         answer = [
             [rbf_kernel_function(d, c, sigma) for d in data] for c in centers
         ]
