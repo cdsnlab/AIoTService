3b0da2f03be477ff5f5fdf8dae3fb2d351c4d6d0
leeth5225 <leeth5225@naver.com>
2022-11-14 18:03:39 +0900

    lapras script

--- script/x_lapras_lambda.sh
+++ script/x_lapras_lambda.sh
@@ -4,16 +4,19 @@
 # lambda_list="0.1"
 random_noise="False"
 # lambda_list="0.1 0.01 0.0001 0.00001"
-# lambda_list="0.001"
-lambda_list="0.1 0.01 0.001 0.0001 0.00001"
+lambda_list="0.00001"
+# lambda_list="0.1 0.01 0.001 0.0001 0.00001"
 model="EARLIEST"
 
 file_name="x_lapras_lambda_basic"
 device="2"
 dataset="lapras"
-nhid="32"
-dropout_rate=""
-batch_size=""
+
+nhid="16"
+# dropout_rate="0.5"
+batch_size="8"
+nepochs="50"
+window_size="5"
 # model_dir="./output/log/220926-154933"
 
 for i in $lambda_list
@@ -29,7 +32,11 @@ do
                         --device $device \
                         --random_noise $random_noise \
                         --model $j \
-                        --dataset $dataset 
+                        --dataset $dataset \
+                        --nhid $nhid \
+                        --batch_size $batch_size \
+                        --nepochs $nepochs \
+                        --window_size $window_size
     done
 done
 
